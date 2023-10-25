4b1ec736d22bdd617bc5bca56fc8c0869c26ae40
evanightly <adamhenderson3x3@gmail.com>
2023-10-26 02:43:42 +0700

    Update port

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ server.get('/test', async (request, reply) => {
 	return 'Test'
 })
 
-const SERVER_PORT: number = 3000
+const SERVER_PORT: number = Number(process.env.PORT || 3000)
 
 server.listen({ port: SERVER_PORT }, (err, address) => {
 	if (err) {
