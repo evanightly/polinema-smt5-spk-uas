ff3db6f76abcc6ad74e0635fb7d40684deb6b48a
evanightly <adamhenderson3x3@gmail.com>
2023-10-26 02:59:38 +0700

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const port: number = Number(process.env.PORT) ?? 3000
-
 app.get('/', (req: any, res: any) => {
 	res.send('Hello World!')
 })
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+app.listen(7000, () => {
+	console.log(`Example app listening on port 7000`)
 })
