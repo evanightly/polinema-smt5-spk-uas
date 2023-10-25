997cbe6558650fe59766c5b84502f831fad02f9e
evanightly <adamhenderson3x3@gmail.com>
2023-10-26 03:44:50 +0700

    Test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,20 @@
-import fastify from 'fastify'
+const fastify = require('fastify')
 
 const server = fastify()
 
 server.register(require('@fastify/cors'))
 
-server.get('/', async (request, reply) => {
+server.get('/', async (request: any, reply: any) => {
 	return 'Ok'
 })
 
-server.get('/test', async (request, reply) => {
+server.get('/test', async (request: any, reply: any) => {
 	return 'Test'
 })
 
-const SERVER_PORT: number = Number(process.env.PORT?? 2121) 
+const SERVER_PORT: number = Number(process.env.PORT ?? 2121)
 
-server.listen({ port: SERVER_PORT }, (err, address) => {
+server.listen({ port: SERVER_PORT }, (err: any, address: any) => {
 	if (err) {
 		console.error(err)
 		process.exit(1)
