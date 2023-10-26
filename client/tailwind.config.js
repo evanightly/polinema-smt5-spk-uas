d810ebab70d7f9414ddce0ba956c09e48eba12a8
evanightly <adamhenderson3x3@gmail.com>
2023-10-27 00:17:43 +0700

    Migrate nextjs to svelte

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('preline/plugin')],
+}
+
